b71050911c8f3ecb4bfffa72068737d39de0b661
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2025-11-16 01:11:56 +0900

    add kiwmil log position sample.

---
+++ example/kiwumil.ts
@@ -0,0 +1,13 @@
+import { TypedDiagram } from "../src/index.ts"
+
+TypedDiagram("Grid of arrange")
+.build((el, rel, hint) => {
+  const [ k, i1, w, u, m, i2, l]
+    = ["k","i","w","u","m","i","l"].map(c => el.core.circle(c))
+  hint.arrangeHorizontal(k, i1, w, u, m, i2, l)
+  hint.alignCenterY(k, i1, w, i2)
+  hint.alignBottom(u, m, l)
+  // uを下段に配置するため、kの下に配置
+  hint.arrangeVertical(k, u)
+  
+}).render(import.meta)
