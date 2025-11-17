b4f5c1f9444f5363d9c30f99126d6578f2d65e01
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2025-11-18 05:54:09 +0900

    chore: refresh guide example

--- example/kiwumil.ts
+++ example/kiwumil.ts
@@ -1,13 +1,17 @@
 import { TypeDiagram } from "../src/index"
 
-TypeDiagram("Grid of arrange")
-.build((el, rel, hint) => {
-  const [ k, i1, w, u, m, i2, l]
-    = ["k","i","w","u","m","i","l"].map(c => el.core.circle(c))
-  hint.arrangeHorizontal(k, i1, w, u, m, i2, l)
-  hint.alignCenterY(k, i1, w, i2)
-  hint.alignBottom(u, m, l)
-  // uを下段に配置するため、kの下に配置
-  hint.arrangeVertical(k, u)
-  
-}).render(import.meta)
+TypeDiagram("Guide builder sample")
+  .build((el, _rel, hint) => {
+    const [k, i1, w, u, m, i2, l] = ["k", "i", "w", "u", "m", "i", "l"].map(c =>
+      el.core.circle(c)
+    )
+
+    hint
+      .createGuideY()
+      .alignBottom(k, i1, w)
+      .alignTop(u, m)
+      .alignBottom(i2)
+      .alignTop(l)
+      .arrange()
+  })
+  .render(import.meta)
